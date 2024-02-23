31dafad0ccb1f095118e9036527fb1c10d51b45e
Ahsan Alamgir <ahsanalamgir14@gmail.com>
2024-02-23 19:18:32 +0500

    Add demo seeder and role seeder

--- database/seeders/App/UserDemoSeeder.php
+++ database/seeders/App/UserDemoSeeder.php
@@ -18,17 +18,88 @@ class UserDemoSeeder extends Seeder
     public function run()
     {
         Role::insert([
+            // [
+            //     'name' => 'Manager',
+            //     'type_id' => 1,
+            //     'created_by' => 1
+            // ],
+            // [
+            //     'name' => 'Moderator',
+            //     'type_id' => 1,
+            //     'created_by' => 1
+            // ],
+            // [
+            //     'name' => 'ADMINISTRATOR',
+            //     'description' => 'System Administrator',
+            //     'type_id' => 1,
+            //     'created_by' => 1,
+            //     'status' => 'ENABLED'
+            // ],
             [
-                'name' => 'Manager',
+                'name' => 'MHEI STAFF',
+                'description' => 'Maritime Higher Education Institutions',
                 'type_id' => 1,
-                'created_by' => 1
+                'created_by' => 1,
+                'status' => 'ENABLED'
             ],
             [
-                'name' => 'Moderator',
+                'name' => 'CHED STAFF',
+                'description' => 'Commission on Higher Education',
                 'type_id' => 1,
-                'created_by' => 1
+                'created_by' => 1,
+                'status' => 'ENABLED'
             ],
+            [
+                'name' => 'SHIPPING COMPANY STAFF',
+                'description' => 'Shipping Company Staff to manage the company vessels',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ],
+            [
+                'name' => 'SHIP MASTER',
+                'description' => 'Vessel\'s Ship Master',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ],
+            [
+                'name' => 'SHIP CHIEF ENGINEER',
+                'description' => 'Vessel\'s Chief Engineer',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ],
+            [
+                'name' => 'SHIP TRAINING OFFICER',
+                'description' => 'Vessel\'s Training Officer',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ],
+            [
+                'name' => 'PCG STAFF',
+                'description' => 'Philippine Coast Guard',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ],
+            [
+                'name' => 'MARINA STAFF',
+                'description' => 'Marina',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ],
+            [
+                'name' => 'STUDENT',
+                'description' => 'Student',
+                'type_id' => 1,
+                'created_by' => 1,
+                'status' => 'ENABLED'
+            ]
         ]);
+
         $permissions = Permission::pluck('id')->toArray();
         $socialLinks = SocialLink::pluck('id')->toArray();
 
